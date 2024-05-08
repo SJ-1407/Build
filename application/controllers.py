bfb622f0d9db72dce6815ed1909b6fd9dd17849e
Shreyas <shreyasjai03@gmail.com>
2024-05-08 10:39:24 +0530

    Products changed again

--- application/controllers.py
+++ application/controllers.py
@@ -1,4 +1,4 @@
-from flask import Flask, request,redirect,url_for,session
+from flask import Flask, request,redirect,url_for,session,flash
 from flask import render_template
 from flask import current_app as app
 from application.database import db
@@ -69,6 +69,7 @@ def register():
                
                db.session.add(user)
                db.session.commit()
+              
                session["username"]=username  #maintaining cookie
                session["user_role"]=role.name
                session["user_email"]=user.email
@@ -80,10 +81,63 @@ def register():
 @app.route("/logout")
 def logout():
     if "username" in session:
+     
         session.pop("username")
         session.pop("user_role")
         session.pop("user_email")
     return redirect("/")
 
+'''@app.route("/buy/<int:product_id>", methods=["GET", "POST"])
+def buy(product_id):
+    product = Product.query.get_or_404(product_id)  # Get the product or return 404 if not found
+    if request.method == "GET":
+        return render_template("buy_product.html", product=product)
+    elif request.method == "POST":
+        if 'user_email' not in session:
+            flash("You need to log in to buy products.", "warning")
+            return redirect(url_for('login'))  # Redirect to login page
+        else:
+           print(session["user_email"])
+
+        user = User.query.filter_by(email=session['user_email']).first() # Retrieve user from session
+        print(user)
+        if not user:
+            flash("User not found.", "error")
+            return redirect(url_for('index'))  # Redirect to home page
+
+        # Assuming 'products' is a relationship field on the User model
+        u_p=user_product(user_id=user.id,product_id=product_id)
+        print(u_p)
+        db.session.add(u_p)
+          # Add the product to the user's collection of bought products
+        db.session.commit()  # Commit the transaction to the database
+
+        flash('Purchase confirmed for ' + product.name, 'success')
+        return redirect(url_for('index'))  # Redirect to the home page or confirmation page
+    return render_template("buy_product.html", product=product)'''
+
+@app.route("/buy/<int:product_id>", methods=["GET", "POST"])
+def buy(product_id):
+    product = Product.query.get_or_404(product_id)  # Get the product or return 404 if not found
+    if request.method == "GET":
+        return render_template("buy_product.html", product=product)
+    elif request.method == "POST":
+        if 'user_email' not in session:
+            flash("You need to log in to buy products.", "warning")
+            return redirect(url_for('login'))  # Redirect to login page
+
+        user = User.query.filter_by(email=session['user_email']).first()  # Retrieve user from session
+        if not user:
+            flash("User not found.", "error")
+            return redirect(url_for('index'))  # Redirect to home page
+
+        # Adding product to the user's list of products using the established relationship
+        
+        user.products.append(product)  # Add the product to the user's collection of bought products
+        db.session.commit()  # Commit the transaction to the database
+           
+        return redirect(url_for('index'))  # Redirect to the home page or confirmation page
+
+    return render_template("buy_product.html", product=product)
 
 
