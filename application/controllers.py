51ebb61d6dea51edbd77a3f31d1fbae6f6dc6168
Harsh Kumar Suryan <89727538+HKSuryan@users.noreply.github.com>
2024-05-09 12:22:40 +0530

    Background and stuff

--- application/controllers.py
+++ application/controllers.py
@@ -144,6 +144,34 @@ def buy(product_id):
 
     return render_template("buy_product.html", product=product)
 
+'''@app.route("/cart/<int:product_id>", methods=["GET", "POST"])
+def buy(product_id):
+    product = Product.query.get_or_404(product_id)  # Get the product or return 404 if not found
+    if request.method == "GET":
+        return render_template("cart_product.html", product=product)
+    elif request.method == "POST":
+        if 'user_email' not in session:
+            flash("You need to log in to buy products.", "warning")
+            return redirect(url_for('login'))  # Redirect to login page
+
+        user = User.query.filter_by(email=session['user_email']).first()  # Retrieve user from session
+        if not user:
+            flash("User not found.", "error")
+            return redirect(url_for('index'))  # Redirect to home page
+
+        # Adding product to the user's list of products using the established relationship
+       
+        flash('Purchase confirmed for ' + product.name, 'success') 
+        user.products.append(product)
+        # Add the product to the user's collection of bought products
+        db.session.commit()  # Commit the transaction to the database
+          
+        
+
+        return redirect(url_for('index'))  # Redirect to the home page or confirmation page
+
+    return render_template("buy_product.html", product=product)'''
+
 
 @app.route('/orders')
 def order_history():
